ac2e07b51ed9f0547f6f6854ce6a648b48f5a12f
Seth Nkwo <sethnkwocool@gmail.com>
2026-05-06 16:51:58 +0100

    Created auth validators

---
+++ applysmart-api/src/validators/auth.validator.ts
@@ -0,0 +1,18 @@
+import {z} from "zod";
+
+const name = z.string().min(1, "Name is required");
+const email = z.string().email().trim().toLowerCase();
+const password = z.string()
+    .min(8, "Password must be at least 8 characters")
+    .max(32, "Password cannot exceed 32 characters");
+
+export const signUpSchema = z.object({
+    name,
+    email,
+    password
+})
+
+export const loginSchema= z.object({
+    email,
+    password
+})
