bc526b22ab912ca67db2e279f272a3d236f71c46
Seth Nkwo <sethnkwocool@gmail.com>
2026-05-09 11:43:28 +0100

    Created auth middleware for protected routes

---
+++ applysmart-api/src/middleware/auth.middleware.ts
@@ -0,0 +1,37 @@
+// Auth middleware
+import { Response, NextFunction } from "express";
+import { AuthRequest } from "../types/express.js";
+import jwt from "jsonwebtoken"
+import { env } from "../config/env.js";
+
+export function protect(req: AuthRequest, res: Response, next: NextFunction) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({
+            message: "No token provided"
+        })
+    };
+
+    const parts = authHeader.split(" ");
+
+    if (parts.length !==2 || parts[0] !== "Bearer") {
+        return res.status(401).json({
+            message: "Invalid token format"
+        })
+    };
+
+    const token = parts[1];
+
+    try {
+        const decoded = jwt.verify(token, env.jwtSecret) as {userId: string}
+
+        req.user = {
+            userId: decoded.userId
+        }
+
+        next()
+    } catch(err) {
+        next(err)
+    }
+}
