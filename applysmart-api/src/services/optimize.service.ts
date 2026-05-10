64b675841f5283a29457f1d731cad37d9094beef
Seth Nkwo <sethnkwocool@gmail.com>
2026-05-10 13:02:20 +0100

    Created service to extract skills from job description

--- applysmart-api/src/services/optimize.service.ts
+++ applysmart-api/src/services/optimize.service.ts
@@ -1,3 +1,29 @@
+import { ISkill, Skill } from "../models/skills.model.js";
+
+// Extract keywords from job description
+export async function extract(jobDescription: string) {
+    const normalized = jobDescription.toLowerCase();
+
+    const skills = await Skill.find()
+
+    const foundSkills: ISkill[] = [];
+
+    for (const skill of skills) {
+        // check if skill name exists in text
+        const hasNameMatch = normalized.includes(skill.name.toLowerCase());
+
+        const hasAliasMatch = skill.aliases.some(alias => 
+            normalized.includes(alias.toLowerCase())
+        )
+
+        if (hasNameMatch || hasAliasMatch) {
+            foundSkills.push(skill)
+        }
+    }
+
+    return foundSkills
+}
+
 // Optimize services
 export async function optimizeCV(cvText: string, jobDescription: string) {
     return {
