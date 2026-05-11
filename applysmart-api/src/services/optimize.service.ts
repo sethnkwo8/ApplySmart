3ee38655b59ce793c6426e5b0a9b95527343741b
Seth Nkwo <sethnkwocool@gmail.com>
2026-05-11 10:43:58 +0100

    Created service to compare cv to extracted skills from jobDescription

--- applysmart-api/src/services/optimize.service.ts
+++ applysmart-api/src/services/optimize.service.ts
@@ -1,7 +1,7 @@
 import { ISkill, Skill } from "../models/skills.model.js";
 
 // Extract keywords from job description
-export async function extract(jobDescription: string) {
+export async function extractKeywords(jobDescription: string) {
     const normalized = jobDescription.toLowerCase();
 
     const skills = await Skill.find()
@@ -10,7 +10,7 @@ export async function extract(jobDescription: string) {
 
     for (const skill of skills) {
         // check if skill name exists in text
-        const hasNameMatch = normalized.includes(skill.name.toLowerCase());
+        const hasNameMatch = normalized.includes(skill.name);
 
         const hasAliasMatch = skill.aliases.some(alias => 
             normalized.includes(alias.toLowerCase())
@@ -22,6 +22,51 @@ export async function extract(jobDescription: string) {
     }
 
     return foundSkills
+}
+
+// Compare cv to job description
+export async function compareCVToJob(cvText: string, extractedKeywords: ISkill[]) {
+    let message: string | undefined
+
+    if (extractedKeywords.length === 0) {
+        message = "No skills found from job description";
+        return {
+            matchedSkills: [],
+            missingSkills: [],
+            score: 0,
+            message
+        };
+    } 
+
+    const normalized = cvText.toLowerCase();
+    const matchedSkills: ISkill[] = [];
+
+    // Create a set of matched IDs
+    const matchedIds = new Set<string>();
+
+    for (const skill of extractedKeywords) {
+        const hasNameMatch = normalized.includes(skill.name.toLowerCase());
+
+        const hasAliasMatch = skill.aliases.some(alias => 
+            normalized.includes(alias.toLowerCase())
+        )
+
+        if (hasNameMatch || hasAliasMatch) {
+            matchedSkills.push(skill)
+            matchedIds.add(skill._id.toString());
+        }
+    }
+
+    const score = (matchedSkills.length / extractedKeywords.length) * 100;
+
+    const missingSkills = extractedKeywords.filter(skill => !matchedIds.has(skill._id.toString()))
+
+    return {
+        matchedSkills,
+        missingSkills,
+        score: Math.round(score),
+        message
+    }
 }
 
 // Optimize services
