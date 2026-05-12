c865389fde2b517ee01ab5f6b9667b09c86a3eb2
Seth Nkwo <sethnkwocool@gmail.com>
2026-05-12 19:11:52 +0100

    Data for hero section

---
+++ applysmart-web/src/lib/landing-page/heroFeatures.tsx
@@ -0,0 +1,7 @@
+import { HeroFeature } from "@/types/landingpage"
+// Hero Features Data
+export const features: HeroFeature[] = [
+        ["Weighted", "skill matching"],
+        ["Instant", "CV analysis"],
+        ["Role-based", "optimization"],
+]
