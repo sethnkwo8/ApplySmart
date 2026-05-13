9d542ed2c10457be79676bfe78258da4746eec83
Seth Nkwo <sethnkwocool@gmail.com>
2026-05-13 21:13:18 +0100

    Created signup api function

---
+++ applysmart-web/src/lib/api/auth.ts
@@ -0,0 +1,22 @@
+// API for auth routes
+import { SignupFormType } from "@/types/auth";
+
+export async function signUpUser(formData: SignupFormType) {
+    const {confirmPassword, ...signUpData} = formData
+    const apiURL = process.env.NEXT_PUBLIC_API_URL;
+
+    const res = await fetch(`${apiURL}/auth/signup`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(signUpData)
+    })
+
+    if (!res.ok) {
+        const errorData = await res.json();
+        throw new Error(errorData.message || "Registration Failed")
+    }
+
+    return await res.json()
+}
