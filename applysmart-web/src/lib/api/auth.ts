89b97f4a0038afaddaec7a6fac68a9248af4b7e9
Seth Nkwo <sethnkwocool@gmail.com>
2026-05-13 21:52:06 +0100

    Created api call for sign in

--- applysmart-web/src/lib/api/auth.ts
+++ applysmart-web/src/lib/api/auth.ts
@@ -1,5 +1,5 @@
 // API for auth routes
-import { SignupFormType } from "@/types/auth";
+import { SigninFormType, SignupFormType } from "@/types/auth";
 
 export async function signUpUser(formData: SignupFormType) {
     const {confirmPassword, ...signUpData} = formData
@@ -11,6 +11,25 @@ export async function signUpUser(formData: SignupFormType) {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(signUpData)
+    })
+
+    if (!res.ok) {
+        const errorData = await res.json();
+        throw errorData
+    }
+
+    return await res.json()
+}
+
+export async function signInUser(formData: SigninFormType) {
+    const apiURL = process.env.NEXT_PUBLIC_API_URL;
+
+    const res = await fetch(`${apiURL}/auth/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
     })
 
     if (!res.ok) {
