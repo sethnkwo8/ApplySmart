d7f7608f1c998a44c8472395d7391dacbd4b91ba
Seth Nkwo <sethnkwocool@gmail.com>
2026-05-14 15:47:25 +0100

    Update auth.service.ts

--- applysmart-api/src/services/auth.service.ts
+++ applysmart-api/src/services/auth.service.ts
@@ -118,6 +118,7 @@ export async function getUser(refreshToken: string) {
         }
 
         return {
+            id: user._id,
             name: user.name,
             email: user.email
         }
