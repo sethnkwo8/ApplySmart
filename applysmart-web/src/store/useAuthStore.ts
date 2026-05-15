000692404dbe7b4f22416296202fbe1826dd4d59
Seth Nkwo <sethnkwocool@gmail.com>
2026-05-15 19:45:45 +0100

    Update useAuthStore.ts

--- applysmart-web/src/store/useAuthStore.ts
+++ applysmart-web/src/store/useAuthStore.ts
@@ -6,6 +6,7 @@ import { AuthState } from "@/types/auth";
 // Initialize the store
 export const useAuthStore = create<AuthState>((set) => ({
     user: null, // When app first boots up nobody is logged in
+    accessToken: null,
     isLoading: true, // Start as true to show loading state
 
     // Login action
@@ -14,12 +15,14 @@ export const useAuthStore = create<AuthState>((set) => ({
         set({isLoading: true});
         try {
             const data = await signInUser(formData);
-            set({user: data.user, isLoading: false}); // set user to signed in user and loading state to false
+            set({user: data.user, accessToken: data.accessToken, isLoading: false}); // set user to signed in user and loading state to false
         } catch(err) {
             set({isLoading: false});
             throw err;
         }
     },
+
+    setUser: (user) => (set({user})),
 
     // Check auth action
     checkAuth: async() => {
@@ -38,7 +41,7 @@ export const useAuthStore = create<AuthState>((set) => ({
         // Set loading state to true
         try{
             await logoutUser();
-            set({user: null}) // Set user to null
+            set({user: null, accessToken: null}) // Set user to null
         } catch(err) {
             console.error("Logout failed:", err)
         }
