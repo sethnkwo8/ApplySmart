a74a5d34ce8b4883d254cfcd6719269bd810951a
Seth Nkwo <sethnkwocool@gmail.com>
2026-05-15 21:16:39 +0100

    Create AuthProvider.tsx

---
+++ applysmart-web/src/components/providers/AuthProvider.tsx
@@ -0,0 +1,16 @@
+// Auth Provider component
+"use client"
+
+import { useAuthStore } from "@/store/useAuthStore";
+import { useEffect } from "react";
+
+export default function AuthProvider({children}: {children: React.ReactNode}) {
+    // Get checkAuth action from useAuthStore
+    const checkAuth = useAuthStore((state) => state.checkAuth)
+
+    // useEffectthat runs on mount
+    useEffect(() => {
+        checkAuth()
+    }, [checkAuth])
+    return <>{children}</>
+}
