56d63533ca7e405f3a607be92e10b13b507c6cc7
Seth Nkwo <sethnkwocool@gmail.com>
2026-05-16 14:25:20 +0100

    Added google client variables

--- applysmart-api/src/config/env.ts
+++ applysmart-api/src/config/env.ts
@@ -9,7 +9,9 @@ const envSchema = z.object({
     MONGO_URI: z.string().min(1),
     JWT_SECRET: z.string().min(1),
     JWT_REFRESH_SECRET: z.string().min(1),
-    NODE_ENV: z.enum(["development", "production", "test"]).default("development")
+    NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
+    GOOGLE_CLIENT_ID: z.string().min(1),
+    GOOGLE_CLIENT_SECRET: z.string().min(1)
 })
 
 const parsed = envSchema.safeParse(process.env);
@@ -24,5 +26,7 @@ export const env = {
     mongoUri: parsed.data.MONGO_URI,
     jwtSecret: parsed.data.JWT_SECRET,
     jwtRefreshSecret: parsed.data.JWT_REFRESH_SECRET,
-    nodeEnv: parsed.data.NODE_ENV
+    nodeEnv: parsed.data.NODE_ENV,
+    googleClientId: parsed.data.GOOGLE_CLIENT_ID,
+    googleClientSecret: parsed.data.GOOGLE_CLIENT_SECRET,
 }
