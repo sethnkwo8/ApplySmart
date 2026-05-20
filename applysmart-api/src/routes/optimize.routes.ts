42fa86d64d7c272642ef8eefcc6e18efddf74f2a
Seth Nkwo <sethnkwocool@gmail.com>
2026-05-20 18:53:21 +0100

    Update optimize.routes.ts

--- applysmart-api/src/routes/optimize.routes.ts
+++ applysmart-api/src/routes/optimize.routes.ts
@@ -1,14 +1,29 @@
 import express from "express"
 import { protect } from "../middleware/auth.middleware.js"
-import { validate } from "../middleware/validator.js"
-import { optimizeSchema } from "../validators/optimize.validator.js"
 import { optimize } from "../controllers/optimize.controller.js"
+import { asyncHandler } from "../utils/asyncHandler.utils.js"
+import { cvUploadMiddleware } from "../middleware/cvUpload.middleware.js"
+import rateLimit from "express-rate-limit";
+import { AuthRequest } from "../types/express.js"
 
 const router = express.Router()
 
 router.use(protect)
 
+// Rate limiting for 5 requests per minute
+const aiOptimizationLimiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minute window
+    max: 5, 
+    keyGenerator: (req: AuthRequest) => req.user?.userId ?? req.ip ?? "", // Used user ID instead of IP-only to prevent accidental multiple users block
+    message: {
+        success: false,
+        message: "You are optimizing resumes too quickly. Please wait a minute before analyzing another CV."
+    },
+    standardHeaders: true, // Return standard rate limit info headers
+    legacyHeaders: false,  // Disable X-RateLimit-* headers
+});
+
 // Optimize route
-router.post("/", validate(optimizeSchema), optimize)
+router.post("/", aiOptimizationLimiter, cvUploadMiddleware, asyncHandler(optimize))
 
 export default router
