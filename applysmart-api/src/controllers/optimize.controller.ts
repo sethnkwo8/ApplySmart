2ba78c31b3eeac670c1ec70e7f740378775ec7bd
Seth Nkwo <sethnkwocool@gmail.com>
2026-05-20 13:23:47 +0100

    Updated optimize controller to use extract text utilities

--- applysmart-api/src/controllers/optimize.controller.ts
+++ applysmart-api/src/controllers/optimize.controller.ts
@@ -3,20 +3,35 @@ import { Response, NextFunction } from "express";
 import { AuthRequest } from "../types/express.js";
 import { AppError } from "../utils/AppError.js";
 import { optimizeCV } from "../services/optimize.service.js";
+import { extractText } from "../utils/textExtractor.js";
 
 export async function optimize(req: AuthRequest, res: Response, next: NextFunction) {
-    const {cvText, jobDescription} = req.body;
-
-    try{
-        if (!req.user) {
-            throw new AppError("Unauthorized", 401)
-        }
-
-        const response = await optimizeCV(cvText, jobDescription);
-
-        res.status(200).json(response)
-
-    } catch(err) {
-        next(err)
+    // Verify user
+    if (!req.user) {
+        throw new AppError("Unauthorized", 401)
     }
+
+    let cvText: string;
+    const {jobDescription} = req.body;
+
+    // Verify job description exists, and either a file OR pasted text
+    if (!jobDescription || (!req.file && !req.body.cvText)) {
+        throw new AppError("Please provide a job description and either upload a CV file or paste your resume text.", 400);
+    }
+
+    // If file is uploaded set CV text to extracted text
+    if (req.file) {
+        cvText = await extractText(req.file.buffer, req.file.mimetype)
+    } else {
+        // Else set CV text to text sent
+        cvText = req.body.cvText;
+    }
+
+    const result = await optimizeCV(cvText, jobDescription)
+
+    res.status(200).json({
+        message: "success",
+        data: result
+    })
+
 }
