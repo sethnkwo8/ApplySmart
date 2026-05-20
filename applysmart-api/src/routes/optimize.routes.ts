be7ab2120c91722040dbfb082743a19e6449d031
Seth Nkwo <sethnkwocool@gmail.com>
2026-05-20 20:42:24 +0100

    Update optimize.routes.ts

--- applysmart-api/src/routes/optimize.routes.ts
+++ applysmart-api/src/routes/optimize.routes.ts
@@ -1,9 +1,9 @@
-import express from "express"
+import express, {Response} from "express"
 import { protect } from "../middleware/auth.middleware.js"
 import { optimize } from "../controllers/optimize.controller.js"
 import { asyncHandler } from "../utils/asyncHandler.utils.js"
 import { cvUploadMiddleware } from "../middleware/cvUpload.middleware.js"
-import rateLimit from "express-rate-limit";
+import rateLimit, {ipKeyGenerator} from "express-rate-limit";
 import { AuthRequest } from "../types/express.js"
 
 const router = express.Router()
@@ -14,7 +14,9 @@ router.use(protect)
 const aiOptimizationLimiter = rateLimit({
     windowMs: 60 * 1000, // 1 minute window
     max: 5, 
-    keyGenerator: (req: AuthRequest) => req.user?.userId ?? req.ip ?? "", // Used user ID instead of IP-only to prevent accidental multiple users block
+    keyGenerator: (req: AuthRequest, res: Response) => {
+        return req.user?.userId ?? ipKeyGenerator(req as any, res as any);
+    }, // Used user ID instead of IP-only to prevent accidental multiple users block
     message: {
         success: false,
         message: "You are optimizing resumes too quickly. Please wait a minute before analyzing another CV."
