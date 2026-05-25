21f696a6bd88cdbca1d7dce5703352fed9d23466
Seth Nkwo <sethnkwocool@gmail.com>
2026-05-25 11:07:53 +0100

    Updated auth middleware to check for cookies

--- applysmart-api/src/middleware/auth.middleware.ts
+++ applysmart-api/src/middleware/auth.middleware.ts
@@ -5,26 +5,25 @@ import jwt from "jsonwebtoken"
 import { env } from "../config/env.js";
 
 export function protect(req: AuthRequest, res: Response, next: NextFunction) {
+    let token = req.cookies?.refreshToken;
+
     const authHeader = req.headers.authorization;
 
-    if (!authHeader) {
+    if (!token && authHeader) {
+        const parts = authHeader.split(" ");
+        if (parts.length === 2 && parts[0] === "Bearer") {
+            token = parts[1];
+        }
+    }
+
+    if (!token) {
         return res.status(401).json({
             message: "No token provided"
-        })
-    };
-
-    const parts = authHeader.split(" ");
-
-    if (parts.length !==2 || parts[0] !== "Bearer") {
-        return res.status(401).json({
-            message: "Invalid token format"
-        })
-    };
-
-    const token = parts[1];
+        });
+    }
 
     try {
-        const decoded = jwt.verify(token, env.jwtSecret) as {userId: string}
+        const decoded = jwt.verify(token, env.jwtRefreshSecret) as {userId: string}
 
         req.user = {
             userId: decoded.userId
